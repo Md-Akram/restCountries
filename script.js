8875c1619a30060f66418ba45cb6becb9b5ca277
Md-Akram <83151706+Md-Akram@users.noreply.github.com>
2022-07-11 21:31:04 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,100 @@
-const parent = document.getElementsByClassName('countries')
+const parent = document.querySelector('.countries');
 
-const getData = () => {
+
+const getCountries = () => {
+
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(countries =>
+        .then((countries) => {
+
             countries.map((country) => {
-                const name = country.name.common;
+                let name = country.name.common;
+                if (name.length > 20) {
+                    let newName = `${name.slice(0, 15)}...`
+                    name = newName;
+                } else {
+                    name = name
+                }
                 const population = country.population;
-                const capital = country.capital === undefined ? ('not found') : (country.capital[0]);
+                let capital = country.capital === undefined ? ('not found') : (country.capital[0]);
+                if (capital.length > 18) {
+                    let newCapital = `${capital.slice(0, 15)}...`
+                    capital = newCapital;
+                } else {
+                    capital = capital
+                }
                 const continents = country.continents[0]
+                const flag = country.flags.png
+                parent.innerHTML += `
+                    <div class="country">
+                        <div class="flag">
+                            <img src="${flag}" alt="flag">
+                        </div>
+                        <div class="info">
+                            <h3>${name}</h3>
+                            <p>Population : <span>${population}</span></p>
+                            <p>region : <span>${continents}</span></p>
+                            <p>capital : <span>${capital}</span></p>
+                        </div>
+                    </div>
+                `
             })
+            changeLoadingState()
+
+        }
         )
 }
 
-getData()
+const changeLoadingState = () => {
+    parent.removeChild(parent.firstElementChild)
+}
 
-                // country.name.common,
-                // country.population
-                // country.continents[0]
 
+
+const getFilteredCountries = (region) => {
+    parent.innerHTML = `<h1 id="loading">Loading...</h1 >`
+
+    fetch(`https://restcountries.com/v3.1/region/${region}`)
+        .then(res => res.json())
+        .then((countries) => {
+            countries.map((country) => {
+                let name = country.name.common;
+                if (name.length > 20) {
+                    let newName = `${name.slice(0, 15)}...`
+                    name = newName;
+                } else {
+                    name = name
+                }
+                const population = country.population;
+                let capital = country.capital === undefined ? ('not found') : (country.capital[0]);
+                if (capital.length > 18) {
+                    let newCapital = `${capital.slice(0, 15)}...`
+                    capital = newCapital;
+                } else {
+                    capital = capital
+                }
+                const continents = country.continents[0]
+                const flag = country.flags.png
+                parent.innerHTML += `
+                    <div class="country">
+                        <div class="flag">
+                            <img src="${flag}" alt="flag">
+                        </div>
+                        <div class="info">
+                            <h3>${name}</h3>
+                            <p>Population : <span>${population}</span></p>
+                            <p>region : <span>${continents}</span></p>
+                            <p>capital : <span>${capital}</span></p>
+                        </div>
+                    </div>
+                `
+            })
+            changeLoadingState()
+
+        }
+        )
+
+}
+
+
+getCountries()
